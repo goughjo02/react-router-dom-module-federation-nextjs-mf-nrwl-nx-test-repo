c1db212f116fe85dbbe298a47f6d0ea54804c721
Joey Gough <j.gough@novafutur.com>
2023-04-26 12:54:02 +0100

    comment out react router dom import for successful build

--- apps/shell/components/HelloWorld.tsx
+++ apps/shell/components/HelloWorld.tsx
@@ -1,10 +1,10 @@
 // react-router-dom
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 export const HelloWorld = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <Link to="/about">About</Link>
+      {/* <Link to="/about">About</Link> */}
     </div>
   );
 };
